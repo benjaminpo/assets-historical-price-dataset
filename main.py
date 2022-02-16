53c69115b213d48b37a703412802fe3c89286649
Benjamin Po <benjaminpo212@gmail.com>
2022-02-16 12:14:26 +0800

    update

--- main.py
+++ main.py
@@ -18,22 +18,23 @@ SPX_CONSTITUENTS_PATH = CONFIG.get('SPX', 'CONSTITUENTS_PATH')
 SPX_CONSTITUENTS_URL = CONFIG.get('SPX', 'CONSTITUENTS_URL')
 DATA_PATH = CONFIG.get('DATA', 'PATH')
 
-def save_spx_components_stock_to_csv():
+def download_and_save(url, path):
     """Return nothing."""
-    req = requests.get(SPX_CONSTITUENTS_URL)
+    req = requests.get(url)
     url_content = req.content
-    csv_file = open(SPX_CONSTITUENTS_PATH, 'wb')
-    csv_file.write(url_content)
-    csv_file.close()
+    file = open(path, 'wb')
+    file.write(url_content)
+    file.close()
 
 
-def save_nasdaq_components_stock_to_csv():
+def save_spx_components_stock_to_file():
     """Return nothing."""
-    req = requests.get(NASDAQ_CONSTITUENTS_URL)
-    url_content = req.content
-    csv_file = open(NASDAQ_CONSTITUENTS_PATH, 'wb')
-    csv_file.write(url_content)
-    csv_file.close()
+    download_and_save(SPX_CONSTITUENTS_URL, SPX_CONSTITUENTS_PATH)
+
+
+def save_nasdaq_components_stock_to_file():
+    """Return nothing."""
+    download_and_save(NASDAQ_CONSTITUENTS_URL, NASDAQ_CONSTITUENTS_PATH)
 
 
 def fetch_spx_components_stock_price(interval = '1d', period = 'max'):
@@ -110,13 +111,13 @@ def main():
         datefmt='%Y-%m-%d %H:%M',
         handlers=[logging.FileHandler(LOGGING_MAIN_PATH, 'w', 'utf-8'), ]
     )
-    save_spx_components_stock_to_csv()
-    save_nasdaq_components_stock_to_csv()
+    save_spx_components_stock_to_file()
+    save_nasdaq_components_stock_to_file()
     fetch_fx_components_stock_price('1d', 'max')
     fetch_fx_components_stock_price('1m', '7d')
-    fetch_nasdaq_components_stock_price('1d')
+    fetch_nasdaq_components_stock_price('1d', 'max')
     fetch_nasdaq_components_stock_price('1m', '7d')
-    fetch_spx_components_stock_price('1d')
+    fetch_spx_components_stock_price('1d', 'max')
     fetch_spx_components_stock_price('1m', '7d')
 
 if __name__ == "__main__":
