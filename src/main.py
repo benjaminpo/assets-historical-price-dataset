72b85b77cb2b3035c09015ac8fdb035d657c3f41
Benjamin Po <benjaminpo212@gmail.com>
2022-02-19 18:15:15 +0800

    refactor

--- src/main.py
+++ src/main.py
@@ -11,9 +11,7 @@ CONFIG.read('config/local.ini')
 FILE_ENCODING = CONFIG.get('FILE', 'ENCODING')
 FX_CONSTITUENTS_PATH = CONFIG.get('FX', 'CONSTITUENTS_PATH')
 NASDAQ_CONSTITUENTS_PATH = CONFIG.get('NASDAQ', 'CONSTITUENTS_PATH')
-NASDAQ_CONSTITUENTS_PATH = CONFIG.get('NASDAQ', 'CONSTITUENTS_PATH')
 NASDAQ_CONSTITUENTS_URL = CONFIG.get('NASDAQ', 'CONSTITUENTS_URL')
-SPX_CONSTITUENTS_PATH = CONFIG.get('SPX', 'CONSTITUENTS_PATH')
 SPX_CONSTITUENTS_PATH = CONFIG.get('SPX', 'CONSTITUENTS_PATH')
 SPX_CONSTITUENTS_URL = CONFIG.get('SPX', 'CONSTITUENTS_URL')
 DATA_PATH = CONFIG.get('DATA', 'PATH')
@@ -42,9 +40,9 @@ def fetch_spx_components_stock_price(interval='1d', period='max'):
     """Return nothing."""
     ticker_list = []
     with open(SPX_CONSTITUENTS_PATH, newline='', encoding=FILE_ENCODING) as file:
-        list = csv.reader(file)
-        next(list)
-        for ticker in list:
+        my_list = csv.reader(file)
+        next(my_list)
+        for ticker in my_list:
             ticker_list.append(ticker[0].replace(".", "-"))
     data = yf.download(
         tickers=ticker_list,
@@ -57,16 +55,16 @@ def fetch_spx_components_stock_price(interval='1d', period='max'):
     data = data.sort_index()
     for ticker in ticker_list:
         filename = ticker.replace("=", "-").replace(".", "-")
-        data.loc[(ticker,),].T.to_csv(DATA_PATH + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
+        data.loc[(ticker,), ].T.to_csv(DATA_PATH + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
 
 
 def fetch_nasdaq_components_stock_price(interval='1d', period='max'):
     """Return nothing."""
     ticker_list = []
     with open(NASDAQ_CONSTITUENTS_PATH, newline='', encoding=FILE_ENCODING) as file:
-        list = csv.reader(file)
-        next(list)
-        for ticker in list:
+        my_list = csv.reader(file)
+        next(my_list)
+        for ticker in my_list:
             ticker_list.append(ticker[0].replace(".", "-"))
     data = yf.download(
         tickers=ticker_list,
@@ -79,16 +77,16 @@ def fetch_nasdaq_components_stock_price(interval='1d', period='max'):
     data = data.sort_index()
     for ticker in ticker_list:
         filename = ticker.replace("=", "-").replace(".", "-")
-        data.loc[(ticker,),].T.to_csv(DATA_PATH + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
+        data.loc[(ticker,), ].T.to_csv(DATA_PATH + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
 
 
 def fetch_fx_components_stock_price(interval='1d', period='max'):
     """Return nothing."""
     ticker_list = []
     with open(FX_CONSTITUENTS_PATH, newline='', encoding=FILE_ENCODING) as file:
-        list = csv.reader(file)
-        next(list)
-        for ticker in list:
+        my_list = csv.reader(file)
+        next(my_list)
+        for ticker in my_list:
             ticker_list.append(ticker[0].replace(".", "-"))
     data = yf.download(
         tickers=ticker_list,
@@ -101,7 +99,7 @@ def fetch_fx_components_stock_price(interval='1d', period='max'):
     data = data.sort_index()
     for ticker in ticker_list:
         filename = ticker.replace("=", "-").replace(".", "-")
-        data.loc[(ticker,),].T.to_csv(DATA_PATH + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
+        data.loc[(ticker,), ].T.to_csv(DATA_PATH + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
 
 
 def main():
