4e60c4cfa42ea1440a45a499db361fa44de6ebbc
Benjamin Po <benjaminpo212@gmail.com>
2022-02-22 19:21:49 +0800

    update

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ def main():
     config = load_config()
     logging_main_path = config['LOGGING']['MAIN_PATH']
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=config['LOGGING']['LEVEL'],
         format='%(asctime)s %(levelname)s %(message)s',
         datefmt='%Y-%m-%d %H:%M',
         handlers=[logging.FileHandler(logging_main_path, 'w', 'utf-8'), ]
     )
-    save_spx_components_stock_to_file()
     save_nasdaq_components_stock_to_file()
+    save_spx_components_stock_to_file()
+
+    fetch_etf_components_stock_price()
     fetch_fx_components_stock_price()
     fetch_nasdaq_components_stock_price()
     fetch_spx_components_stock_price()
-    fetch_etf_components_stock_price()
 
 
 if __name__ == "__main__":
--- pack/util.py
+++ pack/util.py
@@ -18,18 +18,6 @@ def load_config():
     return yaml.safe_load(open('./config/local.yml', 'r'))
 
 
-def save_spx_components_stock_to_file():
-    """Return nothing."""
-    config = load_config()
-    download_and_save(config['SPX']['CONSTITUENTS_URL'], config['SPX']['CONSTITUENTS_PATH'])
-
-
-def save_nasdaq_components_stock_to_file():
-    """Return nothing."""
-    config = load_config()
-    download_and_save(config['NASDAQ']['CONSTITUENTS_URL'], config['NASDAQ']['CONSTITUENTS_PATH'])
-
-
 def fetch_assets(constituents_path, interval, period):
     ticker_list = []
     config = load_config()
@@ -51,36 +39,49 @@ def fetch_assets(constituents_path, interval, period):
     data = data.sort_index()
     for ticker in ticker_list:
         filename = ticker.replace("=", "-").replace(".", "-")
-        data.loc[(ticker,),].T.to_csv(data_path + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
+        data.loc[(ticker,), ].T.to_csv(data_path + interval + '/' + filename + '.csv', sep=',', encoding='utf-8')
+
+
+def fetch_assets_multi_timeframe(constituents_path):
+    fetch_assets(constituents_path, '1d', 'max')
+    fetch_assets(constituents_path, '1m', '7d')
 
 
 def fetch_spx_components_stock_price():
     """Return nothing."""
     config = load_config()
     constituents_path = config['SPX']['CONSTITUENTS_PATH']
-    fetch_assets(constituents_path, '1d', 'max')
-    fetch_assets(constituents_path, '1m', '7d')
+    fetch_assets_multi_timeframe(constituents_path)
 
 
 def fetch_nasdaq_components_stock_price():
     """Return nothing."""
     config = load_config()
     constituents_path = config['NASDAQ']['CONSTITUENTS_PATH']
-    fetch_assets(constituents_path, '1d', 'max')
-    fetch_assets(constituents_path, '1m', '7d')
+    fetch_assets_multi_timeframe(constituents_path)
 
 
 def fetch_fx_components_stock_price():
     """Return nothing."""
     config = load_config()
     constituents_path = config['FX']['CONSTITUENTS_PATH']
-    fetch_assets(constituents_path, '1d', 'max')
-    fetch_assets(constituents_path, '1m', '7d')
+    fetch_assets_multi_timeframe(constituents_path)
 
 
 def fetch_etf_components_stock_price():
     """Return nothing."""
     config = load_config()
     constituents_path = config['ETF']['CONSTITUENTS_PATH']
-    fetch_assets(constituents_path, '1d', 'max')
-    fetch_assets(constituents_path, '1m', '7d')
+    fetch_assets_multi_timeframe(constituents_path)
+
+
+def save_spx_components_stock_to_file():
+    """Return nothing."""
+    config = load_config()
+    download_and_save(config['SPX']['CONSTITUENTS_URL'], config['SPX']['CONSTITUENTS_PATH'])
+
+
+def save_nasdaq_components_stock_to_file():
+    """Return nothing."""
+    config = load_config()
+    download_and_save(config['NASDAQ']['CONSTITUENTS_URL'], config['NASDAQ']['CONSTITUENTS_PATH'])
